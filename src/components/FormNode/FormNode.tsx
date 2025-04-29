53c412177a2dc7afd5c147b467c71b8de6fb85cd
soniikot <soniikot@gmail.com>
2025-04-28 17:50:17 -0700

    create global variables for css

--- src/components/FormNode/FormNode.tsx
+++ src/components/FormNode/FormNode.tsx
@@ -1,5 +1,5 @@
 import {NodeData} from '../../types/types'
-import styles from './styles.module.css';
+import styles from './styles.module.scss';
 import {Handle, Position} from "@xyflow/react";
 
 enum NodeTypes {
